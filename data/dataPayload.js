9a8584487a4625708b9c11843cc1486f4f296b4b
JavierCanoy <javier@mabolotech.com>
2023-03-07 09:15:22 +0800

    delete

--- data/dataPayload.js
+++
@@ -1,9 +0,0 @@
-export default function data() {
-  fetch(`${process.env.NEXT_PUBLIC_URL}/users/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userData),
-  });
-}
